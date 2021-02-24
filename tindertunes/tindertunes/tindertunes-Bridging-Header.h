7b99926908fcd3e9f821e37d59a47a2efea45c69
Sydney Chiang <sjchiang@uci.edu>
2021-02-23 22:21:37 -0500

    changed settings, configured info.plist, linker flags, & bridging header

---
+++ tindertunes/tindertunes/tindertunes-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  tindertunes-Bridging-Header.h
+//  tindertunes
+//
+//  Created by Sydney Chiang on 2/23/21.
+//
+
+#ifndef tindertunes_Bridging_Header_h
+#define tindertunes_Bridging_Header_h
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* tindertunes_Bridging_Header_h */
